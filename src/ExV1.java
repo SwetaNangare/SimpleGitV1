aee93717edf628277431dc2ae8b6f0a36922a3f4
Sweta Nangare <sweta@Swetas-MacBook-Pro.local>
2022-02-11 21:50:04 -0500

    Firstv1 commit

---
+++ src/ExV1.java
@@ -0,0 +1,13 @@
+public class ExV1 {
+    public static void main(String[] args) {
+                String a="Hello";
+                if(a=="Hello"){
+                    System.out.println("Correct");
+                }
+                else {
+                    System.out.println("Something went wrong");
+                }
+            }
+        }
+
+
